bc33a4ab0f91057e02eb0407fb6a009edd4b8706
kclarkedesign <kclarke.design@gmail.com>
2016-02-25 14:08:12 -0500

    tweaked navOpen class so it now longer affects first load display of sidebar overlay, added padding to mobile global search bar, updated navConfig to point to live site nodeID number instead of stage2

--- src/navConfig.js
+++ src/navConfig.js
@@ -3,7 +3,7 @@
 
     angular.module('nav.config', []).constant('navConfig', {
         //server-specific settings go here
-        'FilterNodeNum': '28219'
+        'FilterNodeNum': '28184'
     });
 
 })(angular);
