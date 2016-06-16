e438884377254a9c82a0c462f6528fc5190780d0
dougeff <djfittipaldi@gmail.com>
2016-06-16 11:58:25 -0400

    fixed bugs that occured due to branches being diverged from Kentico upgrade

--- src/navConfig.js
+++ src/navConfig.js
@@ -3,8 +3,9 @@
 
     angular.module('nav.config', []).constant('navConfig', {
         //server-specific settings go here
-        //'FilterNodeNum': '28219'
-        'FilterNodeNum': '28184' //Live Site ID
+        'ActiveFilterNodeNum': '28219',
+        'DefaultFilterNodeNum': '28219'
+        //'FilterNodeNum': '28184' //Live Site ID
     });
 
 })(angular);
